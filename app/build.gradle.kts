a4f8798a4bbcceb00132fdfedc8ff66a835755c0
Egor <109822937+jvqtil@users.noreply.github.com>
2026-09-05 19:49:55 +0300

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.jvqtil.cuber"
         minSdk = 26
         targetSdk = 37
-        versionCode = 110
-        versionName = "1.1.0"
+        versionCode = 120
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
